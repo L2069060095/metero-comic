72e3588b74032844a2231530bc184578906b3f79
WS021227 <2812339731@qq.com>
2022-08-01 13:11:38 +0800

    完成登陆注册，登录成功后存储用户数据到切片中

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -11,32 +11,10 @@ import Nav from "../../components/ws/nav/nav"
 
 const Me: FC<any> = (props): any => {
 
-    const [img,setimg]=useState()
-
     // 通过useSelector钩子函数，来获取store中指定切片中的数据  就相当于vuex中的mapState函数
     const userdatas = useSelector((store: any) => store.ws_userdatas.userdatas)
     // 通过dispatch钩子函数，传入切片中定义的方法，进行数据的操控
     const dispatch = useDispatch()
-
-    //blob文件转化
-    function httpRequest(src: any) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', src, true);
-            xhr.responseType = 'blob';
-            xhr.onload = function (e) {
-                if (this.status == 200) {
-                    let myBlob = this.response;
-                    let files = new window.File([myBlob], myBlob.type, { type: myBlob.type }) // myBlob.type 自定义文件名
-                    resolve(files)
-                    // setimg(files)
-                } else {
-                    reject(false)
-                }
-            };
-            xhr.send();
-        })
-    }
 
     useEffect(() => {
         console.log(userdatas, "切片中的数据")
@@ -44,9 +22,8 @@ const Me: FC<any> = (props): any => {
 
     return (
         <div>
-            <Nav></Nav>
-            <img src={img}></img>       
-            <button onClick={()=>httpRequest(userdatas[0].payload.payload.img[0].url)}>点击获取</button>
+            <Nav></Nav>    
+            <button>点击获取</button>
         </div>
     )
 }
